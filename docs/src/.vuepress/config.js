7a0fe5c7144ad4397c8ddc3d681633d5fe762dd3
fanux <fhtjob@hotmail.com>
2022-05-29 01:39:47 +0800

    add

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,64 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'sealos',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: 'https://user-images.githubusercontent.com/8912557/170836379-9d824d22-0e0e-4f61-a0c3-f7471463f4be.png' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    logo: 'https://user-images.githubusercontent.com/8912557/170836379-9d824d22-0e0e-4f61-a0c3-f7471463f4be.png',
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'github',
+        link: 'https://github.com/labring/sealos'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://user-images.githubusercontent.com/8912557/170836379-9d824d22-0e0e-4f61-a0c3-f7471463f4be.png
+tagline: kubernetes-kernel-based Cloud OS!
+actionText: Quick Start →
+actionLink: /guide/
+features:
+- title: Simple
+  details: Everything in one command
+- title: Flexible
+  details: Combination of any application you need
+- title: Powerful
+  details: Unlimited expansion of various self-operation distributed applications
+footer: Made by fanux with ❤️
+---
